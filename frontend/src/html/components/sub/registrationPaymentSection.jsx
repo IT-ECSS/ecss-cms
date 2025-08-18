f2ba6b56fa2e8b17b77adc5536b4a0eb69949fc4
MosesECSS03 <175521933+MosesECSS03@users.noreply.github.com>
2025-08-18 20:04:10 +0800

    Update registrationPaymentSection.jsx

--- frontend/src/html/components/sub/registrationPaymentSection.jsx
+++ frontend/src/html/components/sub/registrationPaymentSection.jsx
@@ -1716,8 +1716,8 @@ class RegistrationPaymentSection extends Component {
     );
   };
   
-  /*// Custom cell renderer for Payment Method with Buttons
-  paymentMethodRenderer = (params, courseName, location, type) => {
+  // Custom cell renderer for Payment Method with Buttons
+ /* paymentMethodRenderer = (params, courseName, location, type) => {
     const currentPaymentMethod = params.value; // Get the current payment method value
 
     let paymentMethods;
@@ -1757,80 +1757,7 @@ class RegistrationPaymentSection extends Component {
     else
     {
       paymentMethods = [];
-    }*/
-
-    // Custom cell renderer for Payment Method with Buttons
-    paymentMethodRenderer = (params, courseName, location, type) => {
-      const currentPaymentMethod = params.value; // Get the current payment method value
-
-      let paymentMethods;
-      if((type === "NSA") || (type === "Marriage Preparation Programme"))
-      {
-        // List of payment methods
-        if(location === "Pasir Ris West Wellness Centre")
-        {
-          if(courseName !== "Community Ukulele – Mandarin")
-          {
-            // For Marriage Preparation Programme, exclude SkillsFuture
-            if(type === "Marriage Preparation Programme")
-            {
-              paymentMethods = ['PayNow'];
-            }
-            else
-            {
-              paymentMethods = ['PayNow', 'SkillsFuture'];
-            }
-          }
-          else
-          {
-            paymentMethods = ['PayNow'];
-          }
-        }
-        else
-        {
-          if(courseName !== "Community Ukulele – Mandarin")
-          {
-            // For Marriage Preparation Programme, exclude SkillsFuture
-            if(type === "Marriage Preparation Programme")
-            {
-              paymentMethods = ['Cash', 'PayNow'];
-            }
-            else
-            {
-              paymentMethods = ['Cash', 'PayNow', 'SkillsFuture'];
-            }
-          }
-          else
-          {
-            paymentMethods = ['Cash', 'PayNow'];
-          }
-        }
-      }
-      else
-      {
-        paymentMethods = [];
-      }
-
-      // Handle button click to update the payment method in the row
-      const handleButtonClick = (method) => {
-        params.api.getRowNode(params.node.id).setDataValue('paymentMethod', method);
-        console.log('Payment method changed to:', method);
-      };
-
-      return (
-        <div className="payment-method-buttons-container">
-          {paymentMethods.map((method) => (
-            <button
-              key={method}
-              className={`payment-method-button ${method === currentPaymentMethod ? 'active' : ''}`}
-              onClick={() => handleButtonClick(method)}
-            >
-              {method}
-            </button>
-          ))}
-        </div>
-      );
-    };
+    }
 
     // Handle button click to update the payment method in the row
     const handleButtonClick = (method) => {
@@ -1838,6 +1765,75 @@ class RegistrationPaymentSection extends Component {
       console.log('Payment method changed to:', method);
     };
 
+    return (
+      <div className="payment-method-buttons-container">
+        {paymentMethods.map((method) => (
+          <button
+            key={method}
+            className={`payment-method-button ${method === currentPaymentMethod ? 'active' : ''}`}
+            onClick={() => handleButtonClick(method)}
+          >
+            {method}
+          </button>
+        ))}
+      </div>
+    );
+  };*/
+
+    // Custom cell renderer for Payment Method with Buttons
+  paymentMethodRenderer = (params, courseName, location, type) => {
+    const currentPaymentMethod = params.value; // Get the current payment method value
+  
+    let paymentMethods;
+    if(type === "NSA")
+    {
+      // List of payment methods
+      if(location === "Pasir Ris West Wellness Centre")
+      {
+        if(courseName !== "Community Ukulele – Mandarin")
+        {
+          paymentMethods = ['PayNow', 'SkillsFuture'];
+        }
+        else
+        {
+          paymentMethods = ['PayNow'];
+        }
+      }
+      else
+      {
+        if(courseName !== "Community Ukulele – Mandarin")
+        {
+          paymentMethods = ['Cash', 'PayNow', 'SkillsFuture'];
+        }
+        else
+        {
+          paymentMethods = ['Cash', 'PayNow'];
+        }
+      }
+    }
+    else if(type === "Marriage Preparation Programme")
+    {
+      // For Marriage Preparation Programme, exclude SkillsFuture
+      /*if(location === "Pasir Ris West Wellness Centre")
+      {
+        paymentMethods = ['PayNow'];
+      }
+      else
+      {*/
+        paymentMethods = ['Cash', 'PayNow'];
+      //}
+    }
+    else
+    {
+      paymentMethods = [];
+    }
+  
+    // Handle button click to update the payment method in the row
+    const handleButtonClick = (method) => {
+      params.api.getRowNode(params.node.id).setDataValue('paymentMethod', method);
+      console.log('Payment method changed to:', method);
+    };
+  
     return (
       <div className="payment-method-buttons-container">
         {paymentMethods.map((method) => (
