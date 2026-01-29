c23f9415c654551441bf2d660d9b43b0a7872ed3
mossleegermany-personal <it@ecss.org.sg>
2026-01-29 18:21:13 +0800

    ok

--- backend/Controller/QR Code/qrCode.js
+++ backend/Controller/QR Code/qrCode.js
@@ -18,7 +18,7 @@ class QRCodeGenerator {
             const buffer = await QRCode.toBuffer(this.text, this.options);
 
             // Save the buffer to a JPG file
-            const filename = `Digital Literacy For Seniors 数字素养 (Tampines North Community Centre).jpg`;
+            const filename = `中医 – 不和慢性病做朋友 TCM – Don’t be a Friend of Chronic Diseases (CT Hub).jpg`;
             fs.writeFileSync(filename, buffer);
             console.log(`QR code generated and saved as ${filename}`);
         } catch (error) {
@@ -28,5 +28,5 @@ class QRCodeGenerator {
 }
 
 // Usage
-const qrCodeGenerator = new QRCodeGenerator(`https://ecss.org.sg/product/digital-literacy-for-seniors%e6%95%b0%e5%ad%97%e7%b4%a0%e5%85%bbtampines-north-community-centre/`);
+const qrCodeGenerator = new QRCodeGenerator(`https://ecss.org.sg/product/%e4%b8%8d%e5%92%8c%e6%85%a2%e6%80%a7%e7%97%85%e5%81%9a%e6%9c%8b%e5%8f%8btcm-dont-be-a-friend-of-chronic-diseases-ct-hub/`);
 qrCodeGenerator.generate();
