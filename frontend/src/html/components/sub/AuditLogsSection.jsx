d33499e4d371c38acc87c78ecc0bd7f13d00d47e
mossleegermany-personal <235678048+IT-ECSS@users.noreply.github.com>
2026-02-12 14:45:18 +0800

    Update AuditLogsSection.jsx

--- frontend/src/html/components/sub/AuditLogsSection.jsx
+++ frontend/src/html/components/sub/AuditLogsSection.jsx
@@ -7,7 +7,7 @@ import * as XLSX from 'xlsx';
 import "../../../css/sub/auditLogs.css";
 import "../../../css/homePage.css";
 
-// Register AG Grid modules
+// Register AG Grid modulesok
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 const baseURL = window.location.hostname === "localhost" 
