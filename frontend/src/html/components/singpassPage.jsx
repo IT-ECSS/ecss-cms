57c0e7b8257b4715914da1f4cb1ef02bd6cbe9e1
mossleegermany-personal <it@ecss.org.sg>
2026-02-19 16:50:01 +0800

    ok

--- frontend/src/html/components/singpassPage.jsx
+++ frontend/src/html/components/singpassPage.jsx
@@ -88,7 +88,7 @@ class SingpassPage extends Component {
       const backendParUrl = "https://ecss-backend-node.azurewebsites.net/singpass/par";
       
       const parResponse = await axios.post(backendParUrl, {
-        scope: "openid user.identity dob email mobileno name race regadd residentialstatus sex uinfin",
+        scope: "openid dob email mobileno name race regadd residentialstatus sex uinfin",
         //redirect_uri: "http://localhost:3000/callback",
         redirect_uri: "https://salmon-wave-09f02b100.6.azurestaticapps.net/callback",
         state: state,
