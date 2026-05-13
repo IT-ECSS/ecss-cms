a2d2e669a77789175642bd6a3f4c0c61e4bcb0bf
mossleegermany-personal <it@ecss.org.sg>
2026-05-13 14:49:24 +0800

    Update courseCodeMapping.js

--- backend/constants/courseCodeMapping.js
+++ backend/constants/courseCodeMapping.js
@@ -27,8 +27,6 @@ const COURSE_CODE_MAPPING = {
     "Art of Positive Communication builds happy homes": "TGS-2025054487",
     "Joyful Grandparenting": "TGS-2025054491",
     "Hanyu Pinyin & The Three Hundred Tang Poems": "TGS-2025054486",
-    "The Art of Paper Quilling": "TGS-2025054488",
-    "Art of Paper Quiliing": "TGS-2025054488",
     "Art of Paper Quilling": "TGS-2025054488",
     "Nagomi Basic Level 2": "TGS-2025054494",
     "Enhanced Therapeutic Intermediate Watercolour": "TGS-2025054495",
