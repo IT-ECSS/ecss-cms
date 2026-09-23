9b1147af4611c2f7f67a2fc1488b2983fd2dd7c2
ECSS IT <it@ecss.org.sg>
2026-09-24 00:27:53 +0800

    ok Done

--- backend/Controller/QR Code/qrCode.js
+++ backend/Controller/QR Code/qrCode.js
@@ -18,7 +18,7 @@ class QRCodeGenerator {
             const buffer = await QRCode.toBuffer(this.text, this.options);
 
             // Save the buffer to a JPG file
-            const filename = `The Chopsticks People (Pasir Ris West Wellness Centre).jpg`;
+            const filename = `The Chopsticks People (Tampines North Community Centre).jpg`;
             fs.writeFileSync(filename, buffer);
             console.log(`QR code generated and saved as ${filename}`);
         } catch (error) {
@@ -28,5 +28,5 @@ class QRCodeGenerator {
 }
 
 // Usage const filename = `https://ecss.org.sg/product/crafting-connectionspasir-ris-west-wellness-centre/`;
-const qrCodeGenerator = new QRCodeGenerator(`https://ecss.org.sg/product/the-chopsticks-people-prw/`);
+const qrCodeGenerator = new QRCodeGenerator(`https://ecss.org.sg/product/the-chopsticks-peopletampines-north-community-centre/`);
 qrCodeGenerator.generate();
