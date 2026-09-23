9809957494b9978536dd2a32242d8ea92c88d51b
ECSS IT <it@ecss.org.sg>
2026-09-24 01:09:35 +0800

    Done

--- backend/Controller/QR Code/qrCode.js
+++ backend/Controller/QR Code/qrCode.js
@@ -18,7 +18,7 @@ class QRCodeGenerator {
             const buffer = await QRCode.toBuffer(this.text, this.options);
 
             // Save the buffer to a JPG file
-            const filename = `The Chopsticks People (Tampines North Community Centre).jpg`;
+            const filename = `长者照护与意识第三部分：情绪福祉与意识提升 Elder Care & Awareness Part 3: Emotional Well-being and Awareness (Tampines North Community Centre).jpg`;
             fs.writeFileSync(filename, buffer);
             console.log(`QR code generated and saved as ${filename}`);
         } catch (error) {
@@ -28,5 +28,5 @@ class QRCodeGenerator {
 }
 
 // Usage const filename = `https://ecss.org.sg/product/crafting-connectionspasir-ris-west-wellness-centre/`;
-const qrCodeGenerator = new QRCodeGenerator(`https://ecss.org.sg/product/the-chopsticks-peopletampines-north-community-centre/`);
+const qrCodeGenerator = new QRCodeGenerator(`https://ecss.org.sg/product/%e9%95%bf%e8%80%85%e7%85%a7%e6%8a%a4%e4%b8%8e%e6%84%8f%e8%af%86%e7%ac%ac%e4%b8%89%e9%83%a8%e5%88%86%ef%bc%9a%e6%83%85%e7%bb%aa%e7%a6%8f%e7%a5%89%e4%b8%8e%e6%84%8f%e8%af%86%e6%8f%90%e5%8d%87-elder-care/`);
 qrCodeGenerator.generate();
